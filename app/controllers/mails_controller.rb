f9c5b74422d4872157be12f6280d645205f04122
Smoggy <avatar145@ukr.net>
2014-06-18 11:37:23 +0300

    a

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -38,19 +38,19 @@ class MailsController < ApplicationController
 
 
   		emails.each do |email|
-  			task_report = @task_reports.joins(:student).where("'students'.'email' = ?", email.from.first).first
+  			task_report = @task_reports.joins(:student).where("students.email = ?", email.from.first).first
   		
   			if task_report
-  		#		task_report.status =  1
-  		#		attachment = email.attachments.first
-  		#	if attachment
-	  	#			file = StringIO.new(attachment.decoded)
-	  	#			file.class.class_eval { attr_accessor :original_filename, :content_type }
-	  	#			file.original_filename = attachment.filename
-	  	#			file.content_type = attachment.mime_type
-	  	#			task_report.report_file = file
-	  	#		end
-	  	#		task_report.save
+  				task_report.status =  1
+  				attachment = email.attachments.first
+  			if attachment
+	  				file = StringIO.new(attachment.decoded)
+	  				file.class.class_eval { attr_accessor :original_filename, :content_type }
+	  				file.original_filename = attachment.filename
+	  				file.content_type = attachment.mime_type
+	  				task_report.report_file = file
+	  			end
+	  			task_report.save
   			end
   		end
 		redirect_to mails_index_path(task)
