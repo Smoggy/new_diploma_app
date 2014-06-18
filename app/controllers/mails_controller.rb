85cd5f7f7b34f3fa40d1b29a2d49d119147aca15
Smoggy <avatar145@ukr.net>
2014-06-18 11:43:27 +0300

    a

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -42,14 +42,14 @@ class MailsController < ApplicationController
   		
   			if task_report
   				task_report.status =  1
-  		#		attachment = email.attachments.first
-  		#	if attachment
+  				attachment = email.attachments.first
+  				if attachment
 	  	#			file = StringIO.new(attachment.decoded)
 	  	#			file.class.class_eval { attr_accessor :original_filename, :content_type }
 	  	#			file.original_filename = attachment.filename
 	  	#			file.content_type = attachment.mime_type
 	  	#			task_report.report_file = file
-	  	#		end
+	  			end
 	  			task_report.save
   			end
   		end
