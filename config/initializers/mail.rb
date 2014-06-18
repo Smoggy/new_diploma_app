b339694bd7fd9bd5984a8f44fbaca00798583fb6
Smoggy <avatar145@ukr.net>
2014-06-18 11:15:28 +0300

    fix

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 Mail.defaults do
   retriever_method :pop3, :address    => "pop3.ukr.net",
                           :port       => 995,
-                          :user_name  => 'test_mailer@ukr.net',
+                          :user_name  => 'test_mailer',
                           :password   => 'qwerty123',
                           :enable_ssl => true
 end
